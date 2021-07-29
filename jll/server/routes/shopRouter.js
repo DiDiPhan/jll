947d0487ff2305e15115a347b667fa5b4d198fcc
vkhangpham <vkhangpham@gmail.com>
2021-07-29 14:28:45 +0700

    change route 2

--- jll/server/routes/shopRouter.js
+++ jll/server/routes/shopRouter.js
@@ -24,29 +24,4 @@ shopRouter.route('/')
         res.end("Deleting all books.");
     })
 
-shopRouter.route('/:bookName')
-    .all((req, res, next) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        next();
-    })
-    .get((req, res, next) => {
-        res.render('pages/shop');
-    })
-    .put((req, res, next) => {
-        res.statusCode = 403;
-        res.write("Updating books named: " + req.params.bookName + "\n");
-        for (var key of Object.keys(req.body)) {
-            res.write(key + "->" + req.body[key] + "\n");
-        }
-        res.end();
-    })
-    .post((req, res, next) => {
-        res.end("POST operation is not supported on /" + req.params.bookName);
-    })
-    .delete((req, res, next) => {
-        res.end("Deleting " + req.params.bookName);
-    })
-
-
 module.exports = shopRouter;
